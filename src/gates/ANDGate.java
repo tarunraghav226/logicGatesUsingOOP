371532322ae6b723e94113ba566d06603ab58b7a
tarunraghav226 <tarun.raghav226@gmail.com>
2019-10-17 18:21:56 +0530

    added ANDGate definition file

--- src/gates/ANDGate.java
+++ src/gates/ANDGate.java
@@ -20,20 +20,22 @@ public class ANDGate{
     public void setInput2(int input2){this.input2=input2;}
     public int getInput2(){return  this.input2;}
 
-    private  int getOutput(input1,input2){
-        return (input1==0 || input2==0)?0:1;
+    private  int getOutput(int input1,int input2){
+        setInput1(input1);
+        setInput2(input2);
+        return (this.input1==0 || this.input2==0)?0:1;
     }
 
-    private int printOutput(){
-        return  0+"AND"+0+"results"+getOutput(0,0)+"\n"+
-                0+"AND"+1+"results"+getOutput(0,1)+"\n"+
-                1+"AND"+0+"results"+getOutput(1,0)+"\n"+
-                1+"AND"+1+"results"+getOutput(1,1)+"\n";
+    private void printOutput(){
+        System.out.println(0+" AND "+0+" results "+getOutput(0,0)+'\n'+
+                           0+" AND "+1+" results "+getOutput(0,1)+'\n'+
+                           1+" AND "+0+" results "+getOutput(1,0)+'\n'+
+                           1+" AND "+1+" results "+getOutput(1,1)+'\n');
 
     }
 
     public void printTruthTable(){
         System.out.println("----Truth Table For AND Gate----");
-        System.out.println(printOutput());
+        printOutput();
     }
 }
