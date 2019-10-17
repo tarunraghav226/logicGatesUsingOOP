6e46e679771ad4fff0b6a0e6c54c5c4f6ae38378
tarunraghav226 <tarun.raghav226@gmail.com>
2019-10-17 18:36:17 +0530

    completed defintion of NOTGate class

--- src/gates/NOTGate.java
+++ src/gates/NOTGate.java
@@ -4,29 +4,33 @@ public class NOTGate{
     private int input;
 
     public NOTGate(){
-        input1=0;
-        input2=0;
+        input=0;
     }
 
     public NOTGate(int input){
-        this.input1=input;
+        this.input=input;
     }
 
-    public void setInput1(int input){this.input=input;}
-    public int getInput1(){return this.input;}
+    public void setInput(int input){this.input=input;}
+    public int getInput(){return this.input;}
 
     private  int getOutput(int input){
-        setInput1(input);
+        setInput(input);
         return (this.input==1)?0:1;
     }
 
     private void printOutput(){
         System.out.println("NOT "+0+" results "+getOutput(0)+'\n'+
-                           "NOT "+1+" results "+getOutput(1)+'\n';
+                           "NOT "+1+" results "+getOutput(1)+'\n');
     }
 
     public void printTruthTable(){
         System.out.println("----Truth Table For NOT Gate----");
         printOutput();
+    }
+
+    public static void main(String[] args) {
+        NOTGate o =new NOTGate();
+        o.printTruthTable();
     }
 }
